1bbcd088ac7199a686f117d79d22462f3b5f97f7
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2019-05-19 17:14:16 -0700

    Added Android Custom Url Scheme

--- GitTrends.Android/MainActivity.cs
+++ GitTrends.Android/MainActivity.cs
@@ -2,10 +2,13 @@
 using Android.Content.PM;
 using Android.OS;
 using Android.Runtime;
+using Android.Content;
+using AsyncAwaitBestPractices;
 
 namespace GitTrends.Droid
 {
     [Activity(Label = "GitTrends", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [IntentFilter(new string[] { Intent.ActionView }, Categories = new[] { Intent.CategoryDefault, Intent.CategoryBrowsable }, DataSchemes = new[] { "gittrends" })]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Permission[] grantResults)
@@ -21,6 +24,9 @@ namespace GitTrends.Droid
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(savedInstanceState);
+
+            if (Intent?.Data is Android.Net.Uri callbackUri)
+                GitHubAuthenticationService.AuthorizeSession(new System.Uri(callbackUri.ToString())).SafeFireAndForget();
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
