fab2478ffa3023791d4fc0bb3c37736bce6b8e4d
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2019-07-22 09:26:40 -0700

    Add SyncFusion License to SecureStorage

--- GitTrends.Mobile.Shared/Service/SyncFusionService.cs
+++ GitTrends.Mobile.Shared/Service/SyncFusionService.cs
@@ -1,14 +1,38 @@
-﻿namespace GitTrends.Mobile.Shared
+﻿using System.ComponentModel;
+using System.Threading.Tasks;
+using GitTrends.Shared;
+using Xamarin.Essentials;
+
+namespace GitTrends.Mobile.Shared
 {
     public static class SyncFusionService
     {
-        public static void Initialize()
+        public static async Task Initialize()
         {
 #if __IOS__
             Syncfusion.SfChart.XForms.iOS.Renderers.SfChartRenderer.Init();
 #endif
-            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("");
+            string syncFusionLicense;
 
+            try
+            {
+                var syncFusionDto = await AzureFunctionsApiService.GetSyncFusionInformation().ConfigureAwait(false);
+
+                syncFusionLicense = syncFusionDto.License;
+
+                await SaveSyncFusionLicense(syncFusionLicense).ConfigureAwait(false);
+            }
+            catch
+            {
+                syncFusionLicense = await GetSyncFusionLicense().ConfigureAwait(false);
+            }
+
+            if (!string.IsNullOrWhiteSpace(syncFusionLicense))
+                Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense(syncFusionLicense);
         }
+
+        public static Task<string> GetSyncFusionLicense() => SecureStorage.GetAsync(nameof(SyncFusionDTO.License));
+
+        static Task SaveSyncFusionLicense(string license) => SecureStorage.SetAsync(nameof(SyncFusionDTO.License), license);
     }
 }
