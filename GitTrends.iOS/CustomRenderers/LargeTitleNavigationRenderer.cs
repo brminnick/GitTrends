89811bf34934ce1486eda13a6e6de86dad3003d6
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2019-10-05 12:18:03 -0500

    Fixed iOS 13 Large Titles Bug

---
+++ GitTrends.iOS/CustomRenderers/LargeTitleNavigationRenderer.cs
@@ -0,0 +1,28 @@
+﻿using GitTrends.iOS.CustomRenderers;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(NavigationPage), typeof(LargeTitleNavigationRenderer))]
+namespace GitTrends.iOS.CustomRenderers
+{
+    public class LargeTitleNavigationRenderer : NavigationRenderer
+    {
+        public override void ViewWillAppear(bool animated)
+        {
+            base.ViewWillAppear(animated);
+
+            var navigationPage = (NavigationPage)Element;
+            var navigationPageBackgroundColor = navigationPage.BarBackgroundColor;
+
+            NavigationBar.StandardAppearance.BackgroundColor = navigationPageBackgroundColor == Color.Default
+                ? UINavigationBar.Appearance.BarTintColor
+                : navigationPageBackgroundColor.ToUIColor();
+
+            NavigationBar.StandardAppearance.TitleTextAttributes = NavigationBar.TitleTextAttributes;
+            NavigationBar.StandardAppearance.LargeTitleTextAttributes = NavigationBar.LargeTitleTextAttributes;
+
+            NavigationBar.ScrollEdgeAppearance = NavigationBar.StandardAppearance;
+        }
+    }
+}
