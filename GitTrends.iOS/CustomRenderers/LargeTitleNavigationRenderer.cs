4a0e405f1f7fc936b1addd0c33968294ce21adc2
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2019-10-14 16:25:41 -0700

    Increased Microsoft.NET.Sdk.Functions to v1.0.30-beta1

--- GitTrends.iOS/CustomRenderers/LargeTitleNavigationRenderer.cs
+++ GitTrends.iOS/CustomRenderers/LargeTitleNavigationRenderer.cs
@@ -18,12 +18,11 @@ namespace GitTrends.iOS.CustomRenderers
                 var navigationPageBackgroundColor = navigationPage.BarBackgroundColor;
 
                 NavigationBar.StandardAppearance.BackgroundColor = navigationPageBackgroundColor == Color.Default
-                ? UINavigationBar.Appearance.BarTintColor
-                : navigationPageBackgroundColor.ToUIColor();
+                                                                    ? UINavigationBar.Appearance.BarTintColor
+                                                                    : navigationPageBackgroundColor.ToUIColor();
 
                 NavigationBar.StandardAppearance.TitleTextAttributes = NavigationBar.TitleTextAttributes;
                 NavigationBar.StandardAppearance.LargeTitleTextAttributes = NavigationBar.LargeTitleTextAttributes;
-
 
                 NavigationBar.ScrollEdgeAppearance = NavigationBar.StandardAppearance;
             }
