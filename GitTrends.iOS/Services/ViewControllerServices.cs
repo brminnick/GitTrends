a4bd7c7b8d74e85a8443383ebae63f78acd3a6ab
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2020-01-16 10:01:03 -0800

    Update NuGet Packages

--- GitTrends.iOS/Services/ViewControllerServices.cs
+++ GitTrends.iOS/Services/ViewControllerServices.cs
@@ -9,30 +9,7 @@ namespace GitTrends.iOS
 {
     public static class ViewControllerServices
     {
-        public static UIViewController GetVisibleViewController()
-        {
-            UIViewController? viewController = null;
-
-            var window = UIApplication.SharedApplication.KeyWindow;
-
-            if (window.WindowLevel == UIWindowLevel.Normal)
-                viewController = window.RootViewController;
-
-            if (viewController is null)
-            {
-                window = UIApplication.SharedApplication
-                    .Windows
-                    .OrderByDescending(w => w.WindowLevel)
-                    .FirstOrDefault(w => w.RootViewController != null && w.WindowLevel == UIWindowLevel.Normal);
-
-                viewController = window?.RootViewController ?? throw new InvalidOperationException("Could not find current view controller.");
-            }
-
-            while (viewController.PresentedViewController != null)
-                viewController = viewController.PresentedViewController;
-
-            return viewController;
-        }
+        public static UIViewController GetVisibleViewController() => Xamarin.Essentials.Platform.GetCurrentUIViewController();
 
         public static async ValueTask<UIViewController> GetVisibleViewControllerAsync()
         {
