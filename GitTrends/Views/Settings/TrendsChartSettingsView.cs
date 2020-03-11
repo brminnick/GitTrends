f59a8d702ff833307639598696d6716b3de6760e
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2020-03-10 17:14:52 -0700

    Implemented C# Markup

--- GitTrends/Views/Settings/TrendsChartSettingsView.cs
+++ GitTrends/Views/Settings/TrendsChartSettingsView.cs
@@ -2,69 +2,82 @@
 using GitTrends.Mobile.Shared;
 using Syncfusion.XForms.Buttons;
 using Xamarin.Forms;
+using Xamarin.Forms.Markup;
+using static Xamarin.Forms.Markup.GridRowsColumns;
 
 namespace GitTrends
 {
     class TrendsChartSettingsView : ContentView
     {
-        readonly TrendsChartSettingsService _trendsChartSettingsService;
-
-        public TrendsChartSettingsView(TrendsChartSettingsService trendsChartSettingsService)
+        public TrendsChartSettingsView(in TrendsChartSettingsService trendsChartSettingsService)
         {
-            _trendsChartSettingsService = trendsChartSettingsService;
-
-            const double cornerRadius = 5;
-
-            var trendsChartSettingsLabel = new Label
-            {
-                AutomationId = SettingsPageAutomationIds.TrendsChartSettingsLabel,
-                Text = "Preferred Charts",
-                FontAttributes = FontAttributes.Bold,
-                FontSize = 18,
-                VerticalTextAlignment = TextAlignment.Start
-            };
-            trendsChartSettingsLabel.SetDynamicResource(Label.TextColorProperty, nameof(BaseTheme.TrendsChartSettingsLabelTextColor));
-
-            var selectionIndicatorSettings = new SelectionIndicatorSettings
-            {
-                CornerRadius = cornerRadius
-            };
-            selectionIndicatorSettings.SetDynamicResource(SelectionIndicatorSettings.ColorProperty, nameof(BaseTheme.TrendsChartSettingsSelectionIndicatorColor));
-
-            var trendsChartSettingControl = new SfSegmentedControl
-            {
-                AutomationId = SettingsPageAutomationIds.TrendsChartSettingsControl,
-                ItemsSource = TrendsChartConstants.TrendsChartTitles.Values.ToList(),
-                VisibleSegmentsCount = TrendsChartConstants.TrendsChartTitles.Values.Count,
-                CornerRadius = cornerRadius,
-                SelectedIndex = (int)_trendsChartSettingsService.CurrentTrendsChartOption,
-                SelectionIndicatorSettings = selectionIndicatorSettings
-            };
-            trendsChartSettingControl.SetDynamicResource(SfSegmentedControl.BorderColorProperty, nameof(BaseTheme.TrendsChartSettingsBorderColor));
-            trendsChartSettingControl.SetDynamicResource(SfSegmentedControl.FontColorProperty, nameof(BaseTheme.TrendsChartSettingsFontColor));
-            trendsChartSettingControl.SelectionChanged += HandleSelectionChanged;
-
-            var grid = new Grid
+            Content = new Grid
             {
                 RowSpacing = 2,
-                RowDefinitions =
+
+                RowDefinitions = Rows.Define(
+                    (Row.Label, new GridLength(1, GridUnitType.Star)),
+                    (Row.Control, new GridLength(2, GridUnitType.Star))),
+
+                ColumnDefinitions = Columns.Define(new GridLength(1, GridUnitType.Star)),
+
+                Children =
                 {
-                    new RowDefinition { Height = new GridLength(1, GridUnitType.Star) },
-                    new RowDefinition { Height = new GridLength(2, GridUnitType.Star) }
-                },
-                ColumnDefinitions =
-                {
-                    new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) }
+                    new TrendsChartSettingsLabel().Row(Row.Label),
+                    new TrendsCharSettingsControl(trendsChartSettingsService).Row(Row.Control)
                 }
             };
-
-            grid.Children.Add(trendsChartSettingsLabel, 0, 0);
-            grid.Children.Add(trendsChartSettingControl, 0, 1);
-
-            Content = grid;
         }
 
-        void HandleSelectionChanged(object sender, Syncfusion.XForms.Buttons.SelectionChangedEventArgs e) =>
-            _trendsChartSettingsService.CurrentTrendsChartOption = (TrendsChartOption)e.Index;
+        enum Row { Label, Control }
+
+        class TrendsChartSettingsLabel : Label
+        {
+            public TrendsChartSettingsLabel()
+            {
+                AutomationId = SettingsPageAutomationIds.TrendsChartSettingsLabel;
+                Text = "Preferred Charts";
+                FontAttributes = FontAttributes.Bold;
+                FontSize = 18;
+                VerticalTextAlignment = TextAlignment.Start;
+                SetDynamicResource(TextColorProperty, nameof(BaseTheme.TrendsChartSettingsLabelTextColor));
+            }
+        }
+
+        class TrendsCharSettingsControl : SfSegmentedControl
+        {
+            const double cornerRadius = 5;
+            readonly TrendsChartSettingsService _trendsChartSettingsService;
+
+            public TrendsCharSettingsControl(in TrendsChartSettingsService trendsChartSettingsService)
+            {
+                _trendsChartSettingsService = trendsChartSettingsService;
+
+                AutomationId = SettingsPageAutomationIds.TrendsChartSettingsControl;
+                ItemsSource = TrendsChartConstants.TrendsChartTitles.Values.ToList();
+                VisibleSegmentsCount = TrendsChartConstants.TrendsChartTitles.Values.Count;
+                CornerRadius = cornerRadius;
+                SelectedIndex = (int)trendsChartSettingsService.CurrentTrendsChartOption;
+                SelectionIndicatorSettings = new TrendsChartSettingsSelectionIndicatorSettings();
+
+                SetDynamicResource(BorderColorProperty, nameof(BaseTheme.TrendsChartSettingsBorderColor));
+                SetDynamicResource(FontColorProperty, nameof(BaseTheme.TrendsChartSettingsFontColor));
+
+                SelectionChanged += HandleSelectionChanged;
+            }
+
+
+            void HandleSelectionChanged(object sender, Syncfusion.XForms.Buttons.SelectionChangedEventArgs e) =>
+                _trendsChartSettingsService.CurrentTrendsChartOption = (TrendsChartOption)e.Index;
+
+            class TrendsChartSettingsSelectionIndicatorSettings : SelectionIndicatorSettings
+            {
+                public TrendsChartSettingsSelectionIndicatorSettings()
+                {
+                    CornerRadius = cornerRadius;
+                    this.SetDynamicResource(ColorProperty, nameof(BaseTheme.TrendsChartSettingsSelectionIndicatorColor));
+                }
+            }
+        }
     }
 }
