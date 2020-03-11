4a3406706accb0f2ba4198eb4443a12f662ea70b
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2020-03-10 17:38:26 -0700

    Implemented C# Markup

--- GitTrends/Views/Repository/RepositoryDataTemplate.cs
+++ GitTrends/Views/Repository/RepositoryDataTemplate.cs
@@ -2,114 +2,89 @@
 using GitTrends.Shared;
 using ImageCircle.Forms.Plugin.Abstractions;
 using Xamarin.Forms;
+using Xamarin.Forms.Markup;
+using static Xamarin.Forms.Markup.GridRowsColumns;
 
 namespace GitTrends
 {
     class RepositoryDataTemplate : DataTemplate
     {
+        const int circleImageHeight = 90;
+        const int emojiColumnSize = 15;
+        const int statisticColumnSize = 30;
+
         const int _smallFontSize = 12;
 
         public RepositoryDataTemplate() : base(CreateRepositoryDataTemplate)
         {
         }
 
-        static Grid CreateRepositoryDataTemplate()
+        enum Row { TopPadding, Title, Description, DescriptionPadding, Statistics, BottomPadding }
+        enum Column { Avatar, AvatarPadding, Emoji1, Statistic1, Emoji2, Statistic2, Emoji3, Statistic3, RightPadding }
+
+        static Grid CreateRepositoryDataTemplate() => new Grid
         {
-            const int circleImageHeight = 90;
-            const int emojiColumnSize = 15;
-            const int countColumnSize = 30;
+            BackgroundColor = Color.Transparent,
 
-            var image = new CircleImage
+            Padding = new Thickness(2, 0, 5, 0),
+            RowSpacing = 2,
+            ColumnSpacing = 3,
+
+            HorizontalOptions = LayoutOptions.Start,
+            VerticalOptions = LayoutOptions.StartAndExpand,
+
+            RowDefinitions = Rows.Define(
+                (Row.TopPadding, new GridLength(1, GridUnitType.Absolute)),
+                (Row.Title, new GridLength(20, GridUnitType.Absolute)),
+                (Row.Description, new GridLength(45, GridUnitType.Absolute)),
+                (Row.DescriptionPadding, new GridLength(1, GridUnitType.Absolute)),
+                (Row.Statistics, new GridLength(_smallFontSize + 2, GridUnitType.Absolute)),
+                (Row.BottomPadding, new GridLength(5, GridUnitType.Absolute))),
+
+
+            ColumnDefinitions = Columns.Define(
+                (Column.Avatar, new GridLength(circleImageHeight, GridUnitType.Absolute)),
+                (Column.AvatarPadding, new GridLength(2, GridUnitType.Absolute)),
+                (Column.Emoji1, new GridLength(emojiColumnSize, GridUnitType.Absolute)),
+                (Column.Statistic1, new GridLength(statisticColumnSize, GridUnitType.Absolute)),
+                (Column.Emoji2, new GridLength(emojiColumnSize, GridUnitType.Absolute)),
+                (Column.Statistic2, new GridLength(statisticColumnSize, GridUnitType.Absolute)),
+                (Column.Emoji3, new GridLength(emojiColumnSize, GridUnitType.Absolute)),
+                (Column.Statistic3, new GridLength(statisticColumnSize, GridUnitType.Absolute)),
+                (Column.RightPadding, new GridLength(1, GridUnitType.Star))),
+
+            Children =
             {
-                HeightRequest = circleImageHeight,
-                WidthRequest = circleImageHeight,
-                HorizontalOptions = LayoutOptions.Start,
-                VerticalOptions = LayoutOptions.Center
-            };
-            image.SetBinding(CircleImage.SourceProperty, nameof(Repository.OwnerAvatarUrl));
+                new AvatarImage().Row(Row.TopPadding).Column(Column.Avatar).RowSpan(6)
+                    .Bind(Image.SourceProperty, nameof(Repository.OwnerAvatarUrl)),
+                new RepositoryNameLabel().Row(Row.Title).Column(Column.Emoji1).ColumnSpan(7)
+                    .Bind(Label.TextProperty, nameof(Repository.Name)),
+                new RepositoryDescriptionLabel().Row(Row.Description).Column(Column.Emoji1).ColumnSpan(7)
+                    .Bind(Label.TextProperty, nameof(Repository.Description)),
+                new SmallNavyBlueSVGImage("star.svg").Row(Row.Statistics).Column(Column.Emoji1),
 
-            var repositoryNameLabel = new DarkBlueLabel
+                new DarkBlueLabel(_smallFontSize - 1).Row(Row.Statistics).Column(Column.Statistic1)
+                    .Bind(Label.TextProperty, nameof(Repository.StarCount)),
+                new SmallNavyBlueSVGImage("repo_forked.svg").Row(Row.Statistics).Column(Column.Emoji2),
+
+                new DarkBlueLabel(_smallFontSize - 1).Row(Row.Statistics).Column(Column.Statistic2)
+                    .Bind(Label.TextProperty, nameof(Repository.ForkCount)),
+                new SmallNavyBlueSVGImage("issue_opened.svg").Row(Row.Statistics).Column(Column.Emoji3),
+
+                new DarkBlueLabel(_smallFontSize - 1).Row(Row.Statistics).Column(Column.Statistic3)
+                    .Bind(Label.TextProperty, nameof(Repository.IssuesCount))
+            }
+        };
+
+        class AvatarImage : CircleImage
+        {
+            public AvatarImage()
             {
-                FontAttributes = FontAttributes.Bold,
-                HorizontalTextAlignment = TextAlignment.Start,
-                VerticalTextAlignment = TextAlignment.Start,
-                LineBreakMode = LineBreakMode.TailTruncation,
-                HorizontalOptions = LayoutOptions.FillAndExpand
-            };
-            repositoryNameLabel.SetBinding(Label.TextProperty, nameof(Repository.Name));
-
-            var repositoryDescriptionLabel = new DarkBlueLabel
-            {
-                FontSize = _smallFontSize,
-                LineBreakMode = LineBreakMode.WordWrap,
-                VerticalTextAlignment = TextAlignment.Start,
-                FontAttributes = FontAttributes.Italic
-            };
-            repositoryDescriptionLabel.SetBinding(Label.TextProperty, nameof(Repository.Description));
-
-            var starsSVGImage = new SmallNavyBlueSVGImage("star.svg");
-            var starsCountLabel = new DarkBlueLabel(_smallFontSize - 1);
-            starsCountLabel.SetBinding(Label.TextProperty, nameof(Repository.StarCount));
-
-            var forksSVGImage = new SmallNavyBlueSVGImage("repo_forked.svg");
-            var forksCountLabel = new DarkBlueLabel(_smallFontSize - 1);
-            forksCountLabel.SetBinding(Label.TextProperty, nameof(Repository.ForkCount));
-
-            var issuesSVGImage = new SmallNavyBlueSVGImage("issue_opened.svg");
-            var issuesCountLabel = new DarkBlueLabel(_smallFontSize - 1);
-            issuesCountLabel.SetBinding(Label.TextProperty, nameof(Repository.IssuesCount));
-
-            var grid = new Grid
-            {
-                BackgroundColor = Color.Transparent,
-
-                Padding = new Thickness(2, 0, 5, 0),
-                RowSpacing = 2,
-                ColumnSpacing = 3,
-
-                HorizontalOptions = LayoutOptions.Start,
-                VerticalOptions = LayoutOptions.StartAndExpand,
-
-                RowDefinitions = {
-                    new RowDefinition { Height = new GridLength(1, GridUnitType.Absolute) },
-                    new RowDefinition { Height = new GridLength(20, GridUnitType.Absolute) },
-                    new RowDefinition { Height = new GridLength(45, GridUnitType.Absolute) },
-                    new RowDefinition { Height = new GridLength(1, GridUnitType.Absolute) },
-                    new RowDefinition { Height = new GridLength(_smallFontSize + 2, GridUnitType.Absolute) },
-                    new RowDefinition { Height = new GridLength(5, GridUnitType.Absolute) },
-                },
-                ColumnDefinitions = {
-                    new ColumnDefinition { Width = new GridLength(circleImageHeight, GridUnitType.Absolute) },
-                    new ColumnDefinition { Width = new GridLength(2, GridUnitType.Absolute) },
-                    new ColumnDefinition { Width = new GridLength(emojiColumnSize, GridUnitType.Absolute) },
-                    new ColumnDefinition { Width = new GridLength(countColumnSize, GridUnitType.Absolute) },
-                    new ColumnDefinition { Width = new GridLength(emojiColumnSize, GridUnitType.Absolute) },
-                    new ColumnDefinition { Width = new GridLength(countColumnSize, GridUnitType.Absolute) },
-                    new ColumnDefinition { Width = new GridLength(emojiColumnSize, GridUnitType.Absolute) },
-                    new ColumnDefinition { Width = new GridLength(countColumnSize, GridUnitType.Absolute) },
-                    new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) }
-                }
-            };
-
-            grid.Children.Add(image, 0, 0);
-            Grid.SetRowSpan(image, 6);
-
-            grid.Children.Add(repositoryNameLabel, 2, 1);
-            Grid.SetColumnSpan(repositoryNameLabel, 7);
-
-            grid.Children.Add(repositoryDescriptionLabel, 2, 2);
-            Grid.SetColumnSpan(repositoryDescriptionLabel, 7);
-
-            grid.Children.Add(starsSVGImage, 2, 4);
-            grid.Children.Add(starsCountLabel, 3, 4);
-
-            grid.Children.Add(forksSVGImage, 4, 4);
-            grid.Children.Add(forksCountLabel, 5, 4);
-
-            grid.Children.Add(issuesSVGImage, 6, 4);
-            grid.Children.Add(issuesCountLabel, 7, 4);
-
-            return grid;
+                HeightRequest = circleImageHeight;
+                WidthRequest = circleImageHeight;
+                HorizontalOptions = LayoutOptions.Start;
+                VerticalOptions = LayoutOptions.Center;
+            }
         }
 
         class SmallNavyBlueSVGImage : SvgCachedImage
@@ -131,6 +106,29 @@ namespace GitTrends
             {
                 var textColor = (Color)Application.Current.Resources[nameof(BaseTheme.TextColor)];
                 ReplaceStringMap = SvgService.GetColorStringMap(textColor.ToHex());
+            }
+        }
+
+        class RepositoryNameLabel : DarkBlueLabel
+        {
+            public RepositoryNameLabel()
+            {
+                FontAttributes = FontAttributes.Bold;
+                HorizontalTextAlignment = TextAlignment.Start;
+                VerticalTextAlignment = TextAlignment.Start;
+                LineBreakMode = LineBreakMode.TailTruncation;
+                HorizontalOptions = LayoutOptions.FillAndExpand;
+            }
+        }
+
+        class RepositoryDescriptionLabel : DarkBlueLabel
+        {
+            public RepositoryDescriptionLabel()
+            {
+                FontSize = _smallFontSize;
+                LineBreakMode = LineBreakMode.WordWrap;
+                VerticalTextAlignment = TextAlignment.Start;
+                FontAttributes = FontAttributes.Italic;
             }
         }
 
