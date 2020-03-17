5c28bc4ed902d5d60c333369943ef451bb3f02bb
Damiano Curia <damianocuria@Damianos-MacBook-Pro.local>
2020-03-17 11:32:01 +0100

    Added editorconfig support

--- GitTrends.Mobile.Shared/Constants/PageTitles.cs
+++ GitTrends.Mobile.Shared/Constants/PageTitles.cs
@@ -2,7 +2,7 @@
 {
     public static class PageTitles
     {
-		public const string RepositoryPage = "Repositories";
+        public const string RepositoryPage = "Repositories";
         public const string SettingsPage = "Settings";
         public const string ReferringSitesPage = "Referring Sites";
     }
