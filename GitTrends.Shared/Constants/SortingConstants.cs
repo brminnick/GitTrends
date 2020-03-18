b270e3f351b9e95479661cac8f88b1c6d1d4855c
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2020-03-17 17:49:38 -0700

    Set DefaultSortingOption to SortingOption.Trending

--- GitTrends.Shared/Constants/SortingConstants.cs
+++ GitTrends.Shared/Constants/SortingConstants.cs
@@ -5,7 +5,7 @@ namespace GitTrends.Shared
 {
     class SortingConstants
     {
-        public const SortingOption DefaultSortingOption = SortingOption.Views;
+        public const SortingOption DefaultSortingOption = SortingOption.Trending;
 
         readonly static Lazy<Dictionary<SortingOption, string>> _sortingOptionsDictionaryHolder = new Lazy<Dictionary<SortingOption, string>>(CreateSortingDictionary);
 
