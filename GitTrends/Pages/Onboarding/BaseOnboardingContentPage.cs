406104d1c39ae49a0d62153f654a338293d455dd
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2020-04-25 15:30:59 -0700

    Update Margin on Android

--- GitTrends/Pages/Onboarding/BaseOnboardingContentPage.cs
+++ GitTrends/Pages/Onboarding/BaseOnboardingContentPage.cs
@@ -40,7 +40,7 @@ namespace GitTrends
                 Children =
                 {
                     new OpacityOverlay().Row(Row.Image).ColumnSpan(All<Column>()),
-                    CreateImageView().Row(Row.Image).ColumnSpan(All<Column>()).Margin(Device.RuntimePlatform is Device.iOS ? new Thickness(32, 44 + 32, 32, 32) : new Thickness(32)),
+                    CreateImageView().Row(Row.Image).ColumnSpan(All<Column>()).Margin(Device.RuntimePlatform is Device.iOS ? new Thickness(32, 44 + 32, 32, 32) : new Thickness(32,16)),
                     descriptionLayout.Row(Row.Description).ColumnSpan(All<Column>()),
                     new OnboardingIndicatorView(carouselPositionIndex).Row(Row.Indicator).Column(Column.Indicator),
                     new NextLabel(nextButtonText).Row(Row.Indicator).Column(Column.Button),
