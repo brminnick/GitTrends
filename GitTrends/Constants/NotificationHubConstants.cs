48f5b20946a81cbcf4b4d99f3e6605c2d8332735
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2020-05-04 20:21:03 -0700

    Update NotificationHubConstants.cs

--- GitTrends/Constants/NotificationHubConstants.cs
+++ GitTrends/Constants/NotificationHubConstants.cs
@@ -2,7 +2,12 @@
 {
     public static class NotificationHubConstants
     {
+#if !AppStore
+        public const string Name = "GitTrends_Debug";
+        public const string ListenConnectionString = "";
+#else
         public const string Name = "GitTrends";
         public const string ListenConnectionString = "";
+#endif
     }
 }
