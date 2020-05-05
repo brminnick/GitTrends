3394752fba1d6537f2760f9c8da2a6eeb37ba153
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2020-05-04 19:27:22 -0700

    Add NotificationHubConstants

---
+++ GitTrends/Constants/NotificationHubConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace GitTrends
+{
+    public static class NotificationHubConstants
+    {
+        public const string Name = "GitTrends";
+        public const string ListenConnectionString = "";
+    }
+}
