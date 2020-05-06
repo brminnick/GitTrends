fe7d1350117390ae0c333e34ab1cff86da5166e0
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2020-05-06 15:58:04 -0700

    Add AppStoreScreenShotsTest

---
+++ GitTrends.UITests/Tests/AppStoreScreenShots.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Xamarin.UITest;
+
+namespace GitTrends.UITests
+{
+    [TestFixture(Platform.Android, UserType.LoggedIn)]
+    [TestFixture(Platform.iOS, UserType.LoggedIn)]
+    class AppStoreScreenShots : BaseTest
+    {
+        public AppStoreScreenShots(Platform platform, UserType userType) : base(platform, userType)
+        {
+        }
+
+        [Test]
+        public async Task AppStoreScreenShotsTest()
+        {
+            //Arrange
+            var screenRect = App.Query().First().Rect;
+
+            //Act
+            App.Screenshot("Repository Page");
+
+            RepositoryPage.TapSettingsButton();
+            await SettingsPage.WaitForPageToLoad().ConfigureAwait(false);
+
+            App.Screenshot("Settings Page");
+
+            SettingsPage.TapBackButton();
+
+            RepositoryPage.TapRepository(RepositoryPage.VisibleCollection.Skip(2).First().Name);
+
+            await TrendsPage.WaitForPageToLoad().ConfigureAwait(false);
+
+            App.TouchAndHoldCoordinates(screenRect.CenterX, screenRect.CenterY);
+            App.Screenshot("Trends Page");
+
+            TrendsPage.TapReferringSitesButton();
+            await ReferringSitesPage.WaitForPageToLoad().ConfigureAwait(false);
+
+            App.Screenshot("Referring Sites Page");
+
+            //Assert
+        }
+    }
+}
