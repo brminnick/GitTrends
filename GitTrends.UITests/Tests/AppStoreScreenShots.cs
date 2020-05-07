d2c071729983723c4788d5d9dca22f5f8ce11bc1
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2020-05-06 17:22:22 -0700

    Add Dark Mode Screenshots

--- GitTrends.UITests/Tests/AppStoreScreenShots.cs
+++ GitTrends.UITests/Tests/AppStoreScreenShots.cs
@@ -20,26 +20,50 @@ namespace GitTrends.UITests
             var screenRect = App.Query().First().Rect;
 
             //Act
-            App.Screenshot("Repository Page");
-
-            RepositoryPage.TapSettingsButton();
-            await SettingsPage.WaitForPageToLoad().ConfigureAwait(false);
-
-            App.Screenshot("Settings Page");
-
-            SettingsPage.TapBackButton();
+            App.Screenshot("Repository Page Light");
 
             RepositoryPage.TapRepository(RepositoryPage.VisibleCollection.Skip(2).First().Name);
 
             await TrendsPage.WaitForPageToLoad().ConfigureAwait(false);
 
             App.TouchAndHoldCoordinates(screenRect.CenterX, screenRect.CenterY);
-            App.Screenshot("Trends Page");
+            App.Screenshot("Trends Page Light");
 
             TrendsPage.TapReferringSitesButton();
             await ReferringSitesPage.WaitForPageToLoad().ConfigureAwait(false);
 
-            App.Screenshot("Referring Sites Page");
+            App.Screenshot("Referring Sites Page Light");
+
+            ReferringSitesPage.ClosePage();
+            await TrendsPage.WaitForPageToLoad().ConfigureAwait(false);
+
+            TrendsPage.TapBackButton();
+            await RepositoryPage.WaitForPageToLoad().ConfigureAwait(false);
+
+            RepositoryPage.TapSettingsButton();
+            await SettingsPage.WaitForPageToLoad().ConfigureAwait(false);
+
+            App.Screenshot("Settings Page Light");
+
+            SettingsPage.SelectTheme(Mobile.Shared.PreferredTheme.Dark);
+            App.Screenshot("Settings Page Dark");
+
+            SettingsPage.TapBackButton();
+            await RepositoryPage.WaitForPageToLoad().ConfigureAwait(false);
+
+            App.Screenshot("Repository Page Dark");
+
+            RepositoryPage.TapRepository(RepositoryPage.VisibleCollection.Skip(2).First().Name);
+
+            await TrendsPage.WaitForPageToLoad().ConfigureAwait(false);
+
+            App.TouchAndHoldCoordinates(screenRect.CenterX, screenRect.CenterY);
+            App.Screenshot("Trends Page Dark");
+
+            TrendsPage.TapReferringSitesButton();
+            await ReferringSitesPage.WaitForPageToLoad().ConfigureAwait(false);
+
+            App.Screenshot("Referring Sites Page Dark");
 
             //Assert
         }
