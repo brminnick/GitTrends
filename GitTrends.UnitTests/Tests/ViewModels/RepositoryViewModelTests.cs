f10cb3691a858f5ab16794fd4ddafd9ff3592f51
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2020-06-02 15:50:39 -0700

    Create RepositoryViewModelTests.cs

---
+++ GitTrends.UnitTests/Tests/ViewModels/RepositoryViewModelTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+
+namespace GitTrends.UnitTests
+{
+    public class RepositoryViewModelTests
+    {
+        //public event EventHandler<PullToRefreshFailedEventArgs> PullToRefreshFailed
+        //public ICommand PullToRefreshCommand { get; }
+        //public ICommand FilterRepositoriesCommand { get; }
+        //public ICommand SortRepositoriesCommand { get; }
+        //public IReadOnlyList<Repository> VisibleRepositoryList
+
+        //public string EmptyDataViewTitle
+        //public string EmptyDataViewDescription
+
+        //public bool IsRefreshing
+
+        [Test]
+        public async Task FilterRepositoriesCommandTest()
+        {
+            //Arrange
+            var repositoryViewModel = ServiceCollection.ServiceProvider.GetService<RepositoryViewModel>();
+
+
+            //Act
+
+            //Assert
+        }
+
+    }
+}
