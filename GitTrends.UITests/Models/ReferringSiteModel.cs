65ce620ff891ac13971ee807126d21121b726304
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2020-06-12 13:04:36 -0700

    Fix GitTrends.UITest Build Errors

--- GitTrends.UITests/Models/ReferringSiteModel.cs
+++ GitTrends.UITests/Models/ReferringSiteModel.cs
@@ -6,7 +6,7 @@ namespace GitTrends.UITests
     public class ReferringSiteModel : IReferringSiteModel
     {
         public DateTimeOffset DownloadedAt { get; set; }
-        public string Referrer { get; set; }
+        public string Referrer { get; set; } = string.Empty;
         public bool IsReferrerUriValid { get; set; }
         public Uri? ReferrerUri { get; set; }
         public long TotalCount { get; set; }
