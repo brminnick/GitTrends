6fb8a42bd27841aa2b60405a24f8ffbd5ed40c74
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2020-07-08 11:28:04 -0700

    Add More Test Tokens

--- GitTrends.Functions/Functions/GetTestToken.cs
+++ GitTrends.Functions/Functions/GetTestToken.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
@@ -14,39 +15,45 @@ namespace GitTrends.Functions
 {
     class GetTestToken
     {
-        readonly static string _uiTestToken_brminnick = Environment.GetEnvironmentVariable("UITestToken_brminnick") ?? string.Empty;
-        readonly static string _uiTestToken_GitTrends = Environment.GetEnvironmentVariable("UITestToken_GitTrends") ?? string.Empty;
+        readonly static Lazy<List<string>> _testTokenListHolder = new Lazy<List<string>>(() => new List<string>
+        {
+            { Environment.GetEnvironmentVariable("UITestToken_brminnick") ?? string.Empty },
+            { Environment.GetEnvironmentVariable("UITestToken_GitTrends") ?? string.Empty },
+            { Environment.GetEnvironmentVariable("UITestToken_GitTrendsApp") ?? string.Empty },
+            { Environment.GetEnvironmentVariable("UITestToken_TheCodeTraveler") ?? string.Empty }
+        });
 
         readonly GitHubApiV3Service _gitHubApiV3Service;
 
         public GetTestToken(GitHubApiV3Service gitHubApiV3Service) => _gitHubApiV3Service = gitHubApiV3Service;
 
+        List<string> TestTokenList => _testTokenListHolder.Value;
+
         [FunctionName(nameof(GetTestToken))]
         public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequest request, ILogger log)
         {
-            GitHubToken gitHubToken;
-
-            var timeout = TimeSpan.FromSeconds(1);
-            var cancellationTokenSource = new CancellationTokenSource(timeout);
-
-            var gitHubApiResponse_brminnick = await _gitHubApiV3Service.GetGitHubApiResponse(_uiTestToken_brminnick, cancellationTokenSource.Token).ConfigureAwait(false);
-            var brminnickApiRequestsRemaining = GitHubApiService.GetNumberOfApiRequestsRemaining(gitHubApiResponse_brminnick.Headers);
-
-            if (brminnickApiRequestsRemaining > 1000)
+            foreach (var testToken in TestTokenList)
             {
-                gitHubToken = new GitHubToken(_uiTestToken_brminnick, string.Empty, "Bearer");
-            }
-            else
-            {
-                gitHubToken = new GitHubToken(_uiTestToken_GitTrends, string.Empty, "Bearer");
-            }
+                var timeout = TimeSpan.FromSeconds(1);
+                var cancellationTokenSource = new CancellationTokenSource(timeout);
 
-            return new ContentResult
-            {
-                Content = JsonConvert.SerializeObject(gitHubToken),
-                StatusCode = (int)HttpStatusCode.OK,
-                ContentType = "application/json"
+                var gitHubApiResponse = await _gitHubApiV3Service.GetGitHubApiResponse(testToken, cancellationTokenSource.Token).ConfigureAwait(false);
+                var apiRequestsRemaining = GitHubApiService.GetNumberOfApiRequestsRemaining(gitHubApiResponse.Headers);
+
+                if (apiRequestsRemaining > 1000)
+                {
+                    var gitHubToken = new GitHubToken(testToken, GitHubConstants.OAuthScope, "Bearer");
+
+                    return new ContentResult
+                    {
+                        Content = JsonConvert.SerializeObject(gitHubToken),
+                        StatusCode = (int)HttpStatusCode.OK,
+                        ContentType = "application/json"
+                    };
+                }
             };
+
+            return new NotFoundObjectResult("No Valid GitHub Token Found");
         }
     }
 }
