ae3ad3a98d35139dd63a37341708f6cf1277b674
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2020-11-10 15:50:12 -0800

    Ensure Repositories with Views/Clones/Stars Are Prioritized

--- GitTrends.Shared/Services/RepositoryService.cs
+++ GitTrends.Shared/Services/RepositoryService.cs
@@ -6,6 +6,8 @@ namespace GitTrends.Shared
     public static class RepositoryService
     {
         public static IEnumerable<Repository> RemoveForksAndDuplicates(in IEnumerable<Repository> repositoriesList) =>
-              repositoriesList.Where(x => !x.IsFork).OrderByDescending(x => x.DataDownloadedAt).GroupBy(x => x.Name).Select(x => x.FirstOrDefault(x => x.DailyViewsList.Any()) ?? x.First());
+            repositoriesList.Where(x => !x.IsFork).OrderByDescending(x => x.DataDownloadedAt).GroupBy(x => x.Name).Select(x => x.FirstOrDefault(x => DoesContainViewsClonesStarsData(x)) ?? x.First());
+
+        static bool DoesContainViewsClonesStarsData(in Repository repository) => repository.TotalViews > 0 || repository.TotalUniqueViews > 0 || repository.TotalClones > 0 || repository.TotalUniqueClones > 0 || repository.StarCount > 0;
     }
 }
