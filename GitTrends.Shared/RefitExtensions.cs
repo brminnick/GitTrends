8b8916e1cfec776201e6d10edd0b93cbf6947901
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2021-02-17 11:00:39 -0800

    Update RefitExtensions.cs

--- GitTrends.Shared/RefitExtensions.cs
+++ GitTrends.Shared/RefitExtensions.cs
@@ -7,6 +7,7 @@ namespace GitTrends.Shared
 {
     public static class RefitExtensions
     {
+        public static T For<T>(string hostUrl) => RestService.For<T>(hostUrl, GetNewtonsoftJsonRefitSettings());
         public static T For<T>(HttpClient client) => RestService.For<T>(client, GetNewtonsoftJsonRefitSettings());
 
         public static RefitSettings GetNewtonsoftJsonRefitSettings() => new(new NewtonsoftJsonContentSerializer(new JsonSerializerSettings { ContractResolver = new CamelCasePropertyNamesContractResolver() }));
