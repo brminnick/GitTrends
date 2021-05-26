49bc0d55bf272d9c0597aad3e6a69e8589513349
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2021-05-25 18:22:21 -0700

    Add Missing Services

--- GitTrends.Functions/Program.cs
+++ GitTrends.Functions/Program.cs
@@ -5,6 +5,8 @@ using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using GitHubApiStatus.Extensions;
 using GitTrends.Shared;
+using Microsoft.Azure.Storage;
+using Microsoft.Azure.Storage.Blob;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -16,6 +18,7 @@ namespace GitTrends.Functions
     class Program
     {
         readonly static string _token = Environment.GetEnvironmentVariable("UITestToken_brminnick") ?? string.Empty;
+        readonly static string _storageConnectionString = Environment.GetEnvironmentVariable("AzureWebJobsStorage") ?? string.Empty;
 
         static Task Main(string[] args)
         {
@@ -48,9 +51,12 @@ namespace GitTrends.Functions
                         .ConfigurePrimaryHttpMessageHandler(config => new HttpClientHandler { AutomaticDecompression = getDecompressionMethods() })
                         .AddTransientHttpErrorPolicy(builder => builder.WaitAndRetryAsync(3, sleepDurationProvider));
 
+                    services.AddSingleton<NuGetService>();
                     services.AddSingleton<GitHubAuthService>();
                     services.AddSingleton<GitHubApiV3Service>();
+                    services.AddSingleton<BlobStorageService>();
                     services.AddSingleton<GitHubGraphQLApiService>();
+                    services.AddSingleton<CloudBlobClient>(CloudStorageAccount.Parse(_storageConnectionString).CreateCloudBlobClient());
 
                     static TimeSpan sleepDurationProvider(int attemptNumber) => TimeSpan.FromSeconds(Math.Pow(2, attemptNumber));
                     static DecompressionMethods getDecompressionMethods() => DecompressionMethods.Deflate | DecompressionMethods.GZip;
