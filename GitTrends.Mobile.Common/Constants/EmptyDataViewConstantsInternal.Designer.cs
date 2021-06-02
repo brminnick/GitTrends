b0ffd7678b6118eefc50afe591ccb01b50bdb7fb
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2021-06-02 16:33:39 -0700

    Make EmptyDataViewConstantsInternal Internal

--- GitTrends.Mobile.Common/Constants/EmptyDataViewConstantsInternal.Designer.cs
+++ GitTrends.Mobile.Common/Constants/EmptyDataViewConstantsInternal.Designer.cs
@@ -16,7 +16,7 @@ namespace GitTrends.Mobile.Common.Constants {
     [System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    public class EmptyDataViewConstantsInternal {
+    internal class EmptyDataViewConstantsInternal {
         
         private static System.Resources.ResourceManager resourceMan;
         
@@ -27,7 +27,7 @@ namespace GitTrends.Mobile.Common.Constants {
         }
         
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Advanced)]
-        public static System.Resources.ResourceManager ResourceManager {
+        internal static System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.Equals(null, resourceMan)) {
                     System.Resources.ResourceManager temp = new System.Resources.ResourceManager("GitTrends.Mobile.Common.Constants.EmptyDataViewConstantsInternal", typeof(EmptyDataViewConstantsInternal).Assembly);
@@ -38,7 +38,7 @@ namespace GitTrends.Mobile.Common.Constants {
         }
         
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Advanced)]
-        public static System.Globalization.CultureInfo Culture {
+        internal static System.Globalization.CultureInfo Culture {
             get {
                 return resourceCulture;
             }
@@ -47,103 +47,103 @@ namespace GitTrends.Mobile.Common.Constants {
             }
         }
         
-        public static string PleaseLoginAgain {
+        internal static string PleaseLoginAgain {
             get {
                 return ResourceManager.GetString("PleaseLoginAgain", resourceCulture);
             }
         }
         
-        public static string SwipeDownToRefresh_Repositories {
+        internal static string SwipeDownToRefresh_Repositories {
             get {
                 return ResourceManager.GetString("SwipeDownToRefresh_Repositories", resourceCulture);
             }
         }
         
-        public static string SwipeDownToRefresh_ReferringSites {
+        internal static string SwipeDownToRefresh_ReferringSites {
             get {
                 return ResourceManager.GetString("SwipeDownToRefresh_ReferringSites", resourceCulture);
             }
         }
         
-        public static string LoginExpired {
+        internal static string LoginExpired {
             get {
                 return ResourceManager.GetString("LoginExpired", resourceCulture);
             }
         }
         
-        public static string NoFilterMatch {
+        internal static string NoFilterMatch {
             get {
                 return ResourceManager.GetString("NoFilterMatch", resourceCulture);
             }
         }
         
-        public static string NoRepositoriesFound {
+        internal static string NoRepositoriesFound {
             get {
                 return ResourceManager.GetString("NoRepositoriesFound", resourceCulture);
             }
         }
         
-        public static string ClearSearchBarTryAgain {
+        internal static string ClearSearchBarTryAgain {
             get {
                 return ResourceManager.GetString("ClearSearchBarTryAgain", resourceCulture);
             }
         }
         
-        public static string NoReferralsYet {
+        internal static string NoReferralsYet {
             get {
                 return ResourceManager.GetString("NoReferralsYet", resourceCulture);
             }
         }
         
-        public static string Uninitialized {
+        internal static string Uninitialized {
             get {
                 return ResourceManager.GetString("Uninitialized", resourceCulture);
             }
         }
         
-        public static string UnableToRetrieveData {
+        internal static string UnableToRetrieveData {
             get {
                 return ResourceManager.GetString("UnableToRetrieveData", resourceCulture);
             }
         }
         
-        public static string NoTrafficYet {
+        internal static string NoTrafficYet {
             get {
                 return ResourceManager.GetString("NoTrafficYet", resourceCulture);
             }
         }
         
-        public static string ZeroStarsEmptyDataViewDescription1 {
+        internal static string ZeroStarsEmptyDataViewDescription1 {
             get {
                 return ResourceManager.GetString("ZeroStarsEmptyDataViewDescription1", resourceCulture);
             }
         }
         
-        public static string ZeroStarsEmptyDataViewDescription2 {
+        internal static string ZeroStarsEmptyDataViewDescription2 {
             get {
                 return ResourceManager.GetString("ZeroStarsEmptyDataViewDescription2", resourceCulture);
             }
         }
         
-        public static string ZeroStarsEmptyDataViewDescription3 {
+        internal static string ZeroStarsEmptyDataViewDescription3 {
             get {
                 return ResourceManager.GetString("ZeroStarsEmptyDataViewDescription3", resourceCulture);
             }
         }
         
-        public static string Congratulations {
+        internal static string Congratulations {
             get {
                 return ResourceManager.GetString("Congratulations", resourceCulture);
             }
         }
         
-        public static string NoStarsYet {
+        internal static string NoStarsYet {
             get {
                 return ResourceManager.GetString("NoStarsYet", resourceCulture);
             }
         }
         
-        public static string FirstStar {
+        internal static string FirstStar {
             get {
                 return ResourceManager.GetString("FirstStar", resourceCulture);
             }
