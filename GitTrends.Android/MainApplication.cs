83fbe8a4a65909694250868ae702bccb4d4aca01
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2022-03-23 14:24:02 -0700

    Fix Java.Lang.IllegalStateException: Unbalanced enter/exit

--- GitTrends.Android/MainApplication.cs
+++ GitTrends.Android/MainApplication.cs
@@ -11,38 +11,44 @@ namespace GitTrends.Droid
 #if AppStore
     [Application(Debuggable = false)]
 #else
-    [Application(Debuggable = true)]
+	[Application(Debuggable = true)]
 #endif
-    public partial class MainApplication : Application
-    {
-        public MainApplication(IntPtr handle, JniHandleOwnership transfer) : base(handle, transfer)
-        {
+	public partial class MainApplication : Application
+	{
+		public MainApplication(IntPtr handle, JniHandleOwnership transfer) : base(handle, transfer)
+		{
 
-        }
+		}
 
-        public override void OnCreate()
-        {
-            this.ShinyOnCreate(new ShinyStartup(new DeviceNotificationsService_Android()));
-            base.OnCreate();
+		public override void OnCreate()
+		{
+			this.ShinyOnCreate(new ShinyStartup(new DeviceNotificationsService_Android()));
+			base.OnCreate();
 
-            AndroidOptions.DefaultSmallIconResourceName = nameof(Resource.Drawable.icon);
-            AndroidOptions.DefaultColorResourceName = nameof(Resource.Color.colorPrimary);
-            AndroidOptions.DefaultLaunchActivityFlags = AndroidActivityFlags.FromBackground;
+			AndroidOptions.DefaultSmallIconResourceName = nameof(Resource.Drawable.icon);
+			AndroidOptions.DefaultColorResourceName = nameof(Resource.Color.colorPrimary);
+			AndroidOptions.DefaultLaunchActivityFlags = AndroidActivityFlags.FromBackground;
 
-            Xamarin.Essentials.Platform.Init(this);
+			Xamarin.Essentials.Platform.Init(this);
 
-            FFImageLoading.Forms.Platform.CachedImageRenderer.Init(true);
-            FFImageLoading.Forms.Platform.CachedImageRenderer.InitImageViewHandler();
-            var ignore = typeof(FFImageLoading.Svg.Forms.SvgCachedImage);
+			FFImageLoading.Forms.Platform.CachedImageRenderer.Init(true);
+			FFImageLoading.Forms.Platform.CachedImageRenderer.InitImageViewHandler();
+			var ignore = typeof(FFImageLoading.Svg.Forms.SvgCachedImage);
 
-            FFImageLoading.ImageService.Instance.Initialize(new FFImageLoading.Config.Configuration
-            {
-                HttpHeadersTimeout = 60,
-                HttpClient = new System.Net.Http.HttpClient(new AndroidClientHandler
-                {
-                    AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate
-                })
-            });
-        }
-    }
+			// Fix Java.Lang.IllegalStateException: Unbalanced enter/exit
+			// https://github.com/luberda-molinet/FFImageLoading/issues/1492
+			FFImageLoading.ImageService.Instance.Initialize(new FFImageLoading.Config.Configuration
+			{
+				HttpHeadersTimeout = 60,
+				HttpReadTimeout = 60,
+				DiskCacheDuration = TimeSpan.FromDays(14),
+				TryToReadDiskCacheDurationFromHttpHeaders = false,
+				ExecuteCallbacksOnUIThread = true,
+				HttpClient = new System.Net.Http.HttpClient(new AndroidClientHandler
+				{
+					AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate
+				})
+			});
+		}
+	}
 }
