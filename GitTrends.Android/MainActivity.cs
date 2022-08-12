fb748027cf91a5d4fcd148e7f47bf64924309895
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2022-08-11 17:44:01 -0700

    Add splash_theme.xml

--- GitTrends.Android/MainActivity.cs
+++ GitTrends.Android/MainActivity.cs
@@ -14,7 +14,7 @@ using Xamarin.Forms;
 
 namespace GitTrends.Droid
 {
-    [Activity(Label = "GitTrends", Exported = true, Icon = "@mipmap/icon", RoundIcon = "@mipmap/icon_round", Theme = "@style/LaunchTheme", LaunchMode = LaunchMode.SingleTop, MainLauncher = true, ScreenOrientation = ScreenOrientation.Portrait)]
+    [Activity(Label = "GitTrends", Exported = true, Icon = "@mipmap/icon", RoundIcon = "@mipmap/icon_round", Theme = "@style/Theme.SplashScreen", LaunchMode = LaunchMode.SingleTop, MainLauncher = true, ScreenOrientation = ScreenOrientation.Portrait)]
     [IntentFilter(new string[] { Intent.ActionView }, Categories = new[] { Intent.CategoryDefault, Intent.CategoryBrowsable }, DataSchemes = new[] { "gittrends" })]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
@@ -28,8 +28,6 @@ namespace GitTrends.Droid
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
-            this.ShinyOnCreate();
-
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
