95cdf978b323af910f79976a5cc6a1194b411216
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2022-10-04 17:18:55 -0700

    Update AzureConstants.cs

--- GitTrends.Shared/Constants/AzureConstants.cs
+++ GitTrends.Shared/Constants/AzureConstants.cs
@@ -2,10 +2,13 @@
 {
 	public static class AzureConstants
 	{
-		public const string GetTestTokenApiKey = "B8qQh1B1-MfnWrvwhe3z1DxrL1c2cm6ZMV45ot2yYKlyAzFut3wSQQ==";
-		public const string GetAppCenterApiKeysKey = "xuyE4DFW1d56bp_hDVKqb_CeDWocolmUTeMsfxkcDS73AzFuEUiAaQ==";
-		public const string GetSyncFusionInformationApiKey = "WYiUp-Ewo5nVs5RF47XkQCamkFgl_9cSCXiUspGC_eMyAzFuFiCdQQ==";
-		public const string GetNotificationHubInformationApiKey = "SeCakN5St3mtAVKjPAXgb3R4sFnhDshJAB5cN0Np1XEcAzFu3-CpTA==";
-		public const string AzureFunctionsApiUrl = "https://gittrends-functions.azurewebsites.net/api";
+#if AppStore
+#error Missing API Keys
+#endif
+		public const string GetTestTokenApiKey = "";
+		public const string GetAppCenterApiKeysKey = "";
+		public const string GetSyncFusionInformationApiKey = "";
+		public const string GetNotificationHubInformationApiKey = "";
+		public const string AzureFunctionsApiUrl = "https://gittrendsfunctions.azurewebsites.net/api";
 	}
 }
