78fb064d9450f9f0c6d4583b9f323a4f78fa2c97
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2022-10-09 14:55:35 -0700

    Update VideoPlayerView (#548)
    * Use `PropertyName`
    
    * Use Azure Media Services Updated Streaming Formats
    
    * Increase to v2.5.3
    
    * Only Update VisibleRepositoriesCollection on Event when `RefreshState is RefreshState.Succeeded or RefreshState.Uninitialized`
    
    * Update GitHubApiV3ServiceTests.cs
    
    * Revert "Update GitHubApiV3ServiceTests.cs"
    
    This reverts commit b5c21b8f3dec491fe36a7a0db9b58ce826e156aa.

--- GitTrends.Shared/Models/StreamingUrl.cs
+++ GitTrends.Shared/Models/StreamingUrl.cs
@@ -5,8 +5,8 @@
 		public StreamingManifest(string manifestUrl)
 		{
 			ManifestUrl = manifestUrl;
-			HlsUrl = manifestUrl + "(format=m3u8-aapl)";
-			DashUrl = manifestUrl + "(format=mpd-time-csf)";
+			HlsUrl = manifestUrl + "(format=m3u8-cmaf)";
+			DashUrl = manifestUrl + "(format=mpd-time-cmaf)";
 		}
 
 		public string ManifestUrl { get; }
