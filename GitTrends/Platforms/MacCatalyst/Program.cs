cec9a3c7d0e7c4c1c89ca9e94be256054bb416ff
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2024-07-16 12:05:00 -0700

    Remove Unnecessary Code

--- GitTrends/Platforms/MacCatalyst/Program.cs
+++ GitTrends/Platforms/MacCatalyst/Program.cs
@@ -1,15 +1,8 @@
-﻿using ObjCRuntime;
-using UIKit;
+﻿using UIKit;
 
 namespace GitTrends;
 
 public class Program
 {
-	// This is the main entry point of the application.
-	static void Main(string[] args)
-	{
-		// if you want to use a different Application Delegate class from "AppDelegate"
-		// you can specify it here.
-		UIApplication.Main(args, null, typeof(AppDelegate));
-	}
+	static void Main(string[] args) => UIApplication.Main(args, null, typeof(AppDelegate));
 }
